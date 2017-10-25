fc3dd5e530eae9c76a5e5dd9d1e6f41a81a8bbfb
Ronald Willis II <32463687+oggiedoggy12@users.noreply.github.com>
2017-10-25 15:15:36 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import { people } from './people.js';
+import PeopleList from './PeopleList.js';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      people: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({people});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <PeopleList people={this.state.people} />
+      </div>
+    );
+  }
+}
+
+export default App;
