96633cfd25c78531efcbca830879cdb220e758cb
Clara Andrew <claraandrew@Claras-MacBook-Pro.local>
2018-01-10 13:32:04 -0500

    styling change

--- react-friends/src/PeopleList.js
+++ react-friends/src/PeopleList.js
@@ -3,22 +3,15 @@ import './PeopleList.css'
 
 const PeopleList = (props) => {
     return (
-        <table className="table">
-            <tbody className="table-body">
-                <tr>
-                    <th>First</th>
-                    <th>Last</th>
-                    <th>Email</th>
-                </tr> 
+        <div className="people">
                 {props.people.map((profile, index) => {
-                    return <tr key={index} className="table-items">
-                        <td>{profile.first_name}</td>
-                        <td>{profile.last_name}</td>
-                        <td>{profile.email}</td>
-                    </tr>
+                    return <div key={index} className="people-person">
+                        <div>{profile.first_name}</div>
+                        <div>{profile.last_name}</div>
+                        <div>{profile.email}</div>
+                    </div>
                 })}
-            </tbody>
-        </table>
+        </div>
     );
 }
 
