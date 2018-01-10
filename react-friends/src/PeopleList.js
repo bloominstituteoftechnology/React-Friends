29d7ab7bd1c63bf46a0ad79e3935370b34686dee
synsyxx <travis_s92@hotmail.com>
2018-01-10 13:40:22 -0600

    List working/organized/styled some

---
+++ react-friends/src/PeopleList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './PeopleList.css';
+
+export const PeopleList = (props) => {
+    return (
+        <ul className='PeopleList'>
+            {props.people.map(person => {
+                return (
+                    <li className='PeopleList__person'>
+                        <div>{person.first_name}</div>
+                        <div>{person.last_name}</div>
+                        <div className='PeopleList__email'>{person.email}</div>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+} 
+
+
