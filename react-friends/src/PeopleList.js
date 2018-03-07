9bded15ba9c3b7f672fd62138ee5c3211f4eaad9
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-03-07 16:16:12 -0600

    minor adjustments

--- react-friends/src/PeopleList.js
+++ react-friends/src/PeopleList.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import people from './people';
 
 export const PeopleList = props => {
-    const { name, cities } =- props;
     return (
-        <div className= "People_Wrapper">
+        <div>
             {props.people.map((person => {
                 return (
                     <div>
                         <h4>First: {person.first_name}</h4>
                         <h4>Last: {person.last_name}</h4>
                         <div>Email: {person.email}</div>
+                        <div>Occupation: {person.occupation}</div>
                     </div>
                 )
             }))}
