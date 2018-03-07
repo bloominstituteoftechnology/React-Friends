624c8e2ccf5c03a629ea19e8a435aed58eaf910d
unknown <ruiz.james12@yahoo.com>
2018-03-07 15:59:29 -0800

    James Ruiz Completed

---
+++ react-friends/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import { people } from './people.js'
+import { PeopleList } from './PeopleList.js';
+import './PeopleList.css'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      People: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ People : people});
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <h1>Welcome to React</h1>
+          <PeopleList people={this.state.People} />
+      </div>
+    );
+  }
+}
+
+export default App;
