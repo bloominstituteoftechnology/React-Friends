833bdf9241d699b918549392676144816a6d0d9e
Matthew Tomas <matthewebentomas@gmail.com>
2018-03-07 15:58:18 -0500

    Functional with some styling

--- react-friends/src/PeopleList.js
+++ react-friends/src/PeopleList.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import './PeopleList.css';
 
 export const PeopleList = (props) => {
     return (
         <ul className='PeopleList'>
             {props.people.map(person => {
                 return (
-                    <li>
-                        <div>{person.first_name}</div>
-                        <div>{person.last_name}</div>
-                        <div>{person.email}</div>
+                    <li className='PeopleList__person'>
+                        <div>{person.first_name} {person.last_name}</div>
+                        <div className='PeopleList__email'>{person.email}</div>
                     </li>
                 );
             })}
