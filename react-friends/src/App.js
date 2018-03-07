b3ded4c3af46569ee57b5c537425efd1d5344eee
Nick Coultas <nncoultas@yahoo.com>
2018-03-07 12:59:36 -0800

    finished mini sprint

---
+++ react-friends/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { people } from './people.js';
+import PeopleList from './PeopleList.js';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      people: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ people });
+  }
+
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+       <PeopleList people = {this.state.people} />
+      </div>
+    );
+  }
+}
+
+export default App;
