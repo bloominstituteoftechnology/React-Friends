5df3822d4de4ae29b852dbc2c9be85ea4b7bfae9
unknown <adfaris@gmail.com>
2018-03-07 16:28:56 -0500

    Finished working the mini- project

---
+++ react-friends/src/PeopleList.js
@@ -0,0 +1,13 @@
+import React , { Component } from 'react';
+import Person from './Person';
+
+export const PeopleList = (props) => {
+    return (
+        <div>
+            {props.people.map((person => {
+                return <Person key ={person.id} person = {person} />
+
+            }))}
+        </div>  
+        );
+};
