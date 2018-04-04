167e6b8ca5cdb4d969faae0e7debea92a46e6a9d
totsuya <supertatortots@gmail.com>
2018-04-04 15:36:12 -0400

    added peoplelist.js and import

--- react-friends/src/App.js
+++ react-friends/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {people} from './people.js
+import { PeopleList } from './PeopleList'
  class App extends Component {
   constructor() {
     super();
