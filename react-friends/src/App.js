e5646e45956ec48564f24bbebbdf144ea9faab27
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-04-04 15:42:23 -0400

    Imported people from people.js and added People array

--- react-friends/src/App.js
+++ react-friends/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { people } from './people.js';
 
 class App extends Component {
   constructor () {
