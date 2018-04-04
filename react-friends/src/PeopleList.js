a7ef3e9202dc490a5acf35652cdfd39215756d3e
Shawn Stewart <s.stewart0220@gmail.com>
2018-04-04 16:13:12 -0500

    Partial completion of the stretch goal.

--- react-friends/src/PeopleList.js
+++ react-friends/src/PeopleList.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import './PeopleList.css';
 
 export const PeopleList = props => {
     console.log('props', props);
-    return <div>{props.peopleProps.map(p => <div>{p.first_name} {p.last_name} {p.email}</div>)}</div>;
+    return <div>{props.peopleProps.map((p, index) => (
+        <div key={index} className="Person">
+            <div classname="Person__Name">{p.first_name} {p.last_name}</div>
+            <div className="Person__Email"><a href="#">{p.email}</a></div>
+            <button className="Person__Button">Show/Hide Friends</button>
+            <FriendsList friends={p.friends}/>
+        </div>
+    ))}</div>;
+};
+
+const FriendsList = props => {
+    return <div className="Person__Friends">{props.friends.map((f, index) => (
+        <div>{f.firstName} {f.lastName}</div>
+    ))}</div>
 };
