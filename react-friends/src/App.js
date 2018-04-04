3f3328e976c50af230b93f028e40370972239bbc
David A. Morales <you@example.com>
2018-04-04 17:01:48 -0400

    still debugging - adding some changes

--- react-friends/src/App.js
+++ react-friends/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { people } from './people.js';
-import { peoplelist } from './peoplelist.js';
+import { people } from './people';
+import { PeopleList } from './PeopleList';
 
 class App extends Component {
   constructor() {
@@ -18,25 +18,9 @@ class App extends Component {
 
   render() {
     return (
-      <peoplelist people={this.state.people} />
+      <PeopleList people={this.state.people} />
     );
   }  
 }
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
--- react-friends/src/peoplelist.js
+++ react-friends/src/peoplelist.js
@@ -1,9 +1,8 @@
 import React from "react";
 
 const PeopleList = props => {
-  console.log("Props", props);
   // map over peopleProp return a dom elment for each name in list
-  return <div>{props.peopleProp.map(c => <div>{c.people}</div>)}</div>;
+  return <div>{props.peopleProp.map(c => <div>{c.names}</div>)}</div>;
 };
 
 export default PeopleList;
