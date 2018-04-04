f6f0aa49dcc98d922a4f2ec55de1f60dd9a6ed83
Jared Cuffe <jcuffe@gmail.com>
2018-04-04 13:27:46 -0700

    Tidied

--- src/PeopleList.js
+++ src/PeopleList.js
@@ -2,16 +2,8 @@ import React from 'react';
 import Person from './Person.js';
 
 export default class PeopleList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = props;
-    console.log('props v');
-    console.log(props);
-    console.log('state v');
-    console.log(this.state);
-  }
 
-  peopleElements = () => this.state.people.map((person) => <Person {...person} />);
+  peopleElements = () => this.props.people.map((person) => <Person {...person} />);
 
   render() {
     return (
