7871d849a713f586eca1f5403f086911de87d18a
Yasin Shuman <email@yasin.io>
2018-04-04 13:45:48 -0700

    completed peoplelist

--- react-friends/src/PeopleList.js
+++ react-friends/src/PeopleList.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const PeopleList = (props) => {
-    // console.log(props);
-    console.log(props.people);
-    return (
+const PeopleList = props => {
+  // console.log(props);
+  console.log(props.people);
+  return (
+    <div>
+      {props.people.map(person => (
         <div>
-        {props.people.map(person => (
-            <div>
-                {person.first_name}
-            </div>
-        )}
+          <div>{person.first_name}</div>
+          <div>{person.last_name}</div>
+          <div>{person.email}</div>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
 export default PeopleList;
