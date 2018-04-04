5143346a806ddbb4e1a0504a196d77704a908b74
Boredom <macnwandu@gmail.com>
2018-04-04 16:21:59 -0500

    finished

--- react-friends/src/PeopleList.js
+++ react-friends/src/PeopleList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { people } from './people';
+import './PeopleList.css';
 
 export const PeopleList = (props) => {
     return (
-        <div>
+        <div className="people-wrapper">
             {props.people.map((person => {
                 return (
-                    <div>
+                    <div className="person-wrapper">
                         <h3>{person.first_name} {person.last_name}</h3>
                         <h4>{person.email}</h4>
                     </div>
