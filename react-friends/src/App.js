3d5ff7e69bc16e1673018f2591d7683adf0282af
Chris <cjmarsh725@gmail.com>
2018-04-04 13:42:10 -0700

    Initial commit

--- react-friends/src/App.js
+++ react-friends/src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { people } from './people.js';
+import PeopleList from './PeopleList';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {'people' : []};
+  }
+
+  componentDidMount(){
+   this.setState({ people: people });
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +24,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <PeopleList people={this.state.people} />
       </div>
     );
   }
