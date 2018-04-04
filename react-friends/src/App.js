a6fcc54907833af32631765b0a9cb7de1dd165df
axhon <axhonruiz@gmail.com>
2018-04-04 10:10:06 -1000

    list is being populated

--- react-friends/src/App.js
+++ react-friends/src/App.js
@@ -1,8 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { people } from "./people";
+import { PeopleList } from "./PeopleList";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState(prevState => ({ people }));
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +26,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <PeopleList people={this.state.people} />
       </div>
     );
   }
