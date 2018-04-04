85073ed7012e367bd955a31b79556fb97ca82ded
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-04-04 15:53:11 -0400

    Imported PeopleList to App, created PeopleList and function

--- react-friends/src/App.js
+++ react-friends/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { people } from './people.js';
+import { PeopleList } from './PeopleList.js';
 
 class App extends Component {
   constructor () {
