968f9b18c61cdcd2eeac834ba5680ad65b4ea816
Devon Bacon <devon.bacon@gmail.com>
2018-04-04 13:57:01 -0700

    Linked App.js to peopoeple.js and PeopleList.js

---
+++ react-friends/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { people } from './people.js';
+import PeopleList from './PeopleList.js';
+
+class App extends Component {
+
+}
+
+export default App;
