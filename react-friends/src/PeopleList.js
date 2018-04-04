0d55d8c75364d3ae04a9291b2b96c150eec0936a
Kyle Kauzlarich <kauzlarich.kyle@gmail.com>
2018-04-04 13:36:44 -0700

    made it to a road block cant figure out how exactly to access the data while its set no use saving to browser till then, refactoring later will link to this commit in the pull request

---
+++ react-friends/src/PeopleList.js
@@ -0,0 +1,9 @@
+import React from "react";
+export const PeopleList = props => {
+    return (
+      <div>
+         {/* props.pep.map((first_name) => ( */}
+            <div>{this.props.pep}</div>
+      </div>
+    );
+  };
