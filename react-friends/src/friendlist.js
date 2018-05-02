57e484e8f0b02ab7877b08b05936823add4305f1
Zach Campbell <beazlebubbles@gmail.com>
2018-05-02 14:55:09 -0600

    Moved keys to kill warnings

--- react-friends/src/friendlist.js
+++ react-friends/src/friendlist.js
@@ -3,7 +3,7 @@ import React from 'react';
 const FriendList = props => {
   return (
     <div>
-      {props.myFriends.map((val, index) => <div className="Friend-entry"><div key={index} className="Friend-person">{val.firstName} {val.lastName} </div></div>)}
+      {props.myFriends.map((val, index) => <div key={index} className="Friend-entry"><div className="Friend-person">{val.firstName} {val.lastName} </div></div>)}
     </div>
   );
 };
