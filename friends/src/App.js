ca9e043a730c34f015bb37ea25a371e3ff0fbe7f
kelly <kelfro@fastmail.com>
2018-05-02 14:19:25 -0700

    got dropdown menu working

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import FriendsList from "./FriendsList";
 import './App.css';
 import { people } from "./people";
+import Dropdown from "./Dropdown";
 
 class App extends Component {
   constructor () {
@@ -20,6 +21,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Friends</h1>
+	  <Dropdown />
         </header>
 	<FriendsList
 	  friends={this.state.friends}/>
