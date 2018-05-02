324a4742fee7bcf9384bc71a45c017a12c68452e
Zach Campbell <beazlebubbles@gmail.com>
2018-05-02 14:25:25 -0600

    Added padding between person entries

--- react-friends/src/peoplelist.js
+++ react-friends/src/peoplelist.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PeopleList = props => {
   return (
     <div>
-      {props.myPeople.map((val, index) => <div key={index} >{val.first_name} {val.last_name}</div>)}
+      {props.myPeople.map((val, index) => <div key={index} className="List-person">{val.first_name} {val.last_name} <div>{val.email}</div></div>)}
     </div>);
 };
 
