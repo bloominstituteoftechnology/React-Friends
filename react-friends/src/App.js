56c213094a0f14a6c12f1c116feb53c52f33006e
Zach Campbell <beazlebubbles@gmail.com>
2018-05-02 13:40:57 -0600

    created new react app

---
+++ react-friends/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      freinds: []
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
