daff1cba3f4975f4ce4ba614e8797256711915cc
Anne Courtney <annecourtney_1@yahoo.com>
2018-05-02 15:08:25 -0500

    set up app.js and peoplelist.js files and got the name array to show up in my app.

---
+++ react-friends/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PeopleList from './PeopleList';
+import logo from './logo.svg';
+import './App.css';
+import { people } from "./people";
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ friends: people })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Friends</h1>
+        </header>
+        <PeopleList
+          friends={this.state.friends}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
