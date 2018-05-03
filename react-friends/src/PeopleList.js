9ad71da63ccab440c229c7c79b640119b2ab642d
AmyShackles <amyshackles@gmail.com>
2018-05-03 11:08:09 -0700

    Deleted Dropdown.js and realized that I was using a div unecessarily, so removed it

--- react-friends/src/PeopleList.js
+++ react-friends/src/PeopleList.js
@@ -13,12 +13,11 @@ const PeopleList = props => {
                                 {friend.first_name} {friend.last_name} </CardTitle>
                                 <CardText>{friend.email}</CardText>
                                  </CardBody>
-                                        <select className="mein-problem_children" caret>
-                                            Friends                               
+                                        <select className="mein-problem_children" caret>                            
                                         {friend.friends.map(friendofPerson => {
                                             return (
                                         <option key={"".concat(friendofPerson.firstName, friendofPerson.lastName)} className="mein-problem-child">
-                                        <div className="meinProblemChild">{friendofPerson.firstName}{friendofPerson.lastName}</div>
+                                        {friendofPerson.firstName}{friendofPerson.lastName}
                                         </option>
                                             )})}
                                         </select>
