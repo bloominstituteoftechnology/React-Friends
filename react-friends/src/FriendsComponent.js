e3db9cc9f053fcf1a2b286328b332ba7ad370b30
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-05-03 11:06:48 -0400

    Dropdown fixed without css only React Components

--- react-friends/src/FriendsComponent.js
+++ react-friends/src/FriendsComponent.js
@@ -6,22 +6,39 @@ class FriendsComponent extends Component {
     super(props);
     this.state = {
       person: props.person,
-      friends: props.person.friends
+      friends: props.person.friends,
+      isHidden: false
     };
+  }
+
+  toggleHidden() {
+    this.setState({
+    isHidden: !this.state.isHidden
+    })
   }
 
   render() {
     return (
+      <div key={this.state.person.first_name + this.state.person.last_name}>
+        {/* anytime we have an array of elements, REACT NEEDS A KEY! for each elem*/}
+        <div onClick={this.toggleHidden.bind(this)} className="person">
+          <div>{this.state.person.first_name} {this.state.person.last_name}</div>
+          <div>{this.state.person.email}</div>
+          {!this.state.isHidden && <div className="toggle fa fa-caret-square-o-down"></div>}
+          {this.state.isHidden && <div className="toggle fa fa-caret-square-o-up"></div>}
+
+        </div>
         <div className="friend" >
         {this.state.friends.map(person => {
           return (
-            <div key={person.firstName + person.lastName}>
+            this.state.isHidden && <div key={person.firstName + person.lastName}>
               {/* anytime we have an array of elements, REACT NEEDS A KEY! for each elem*/}
               {person.firstName} {person.lastName}
             </div>
           );
         })}
         </div>
+      </div>
     )
   }
 }
--- react-friends/src/PeopleList.js
+++ react-friends/src/PeopleList.js
@@ -6,27 +6,15 @@ class PeopleList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isHidden: false,
     }
   }
-  toggleHidden() {
-    this.setState({
-    isHidden: !this.state.isHidden
-    })
-  }
+
   render() {
     return (
       <div>
         {this.props.people.map(person => {
           return (
-            <div key={person.first_name + person.last_name}>
-              {/* anytime we have an array of elements, REACT NEEDS A KEY! for each elem*/}
-              <div onClick={this.toggleHidden.bind(this)} className="person">
-                <div>{person.first_name} {person.last_name}</div>
-                <div>{person.email}</div>
-              </div>
-              {!this.state.isHidden && <FriendsComponent person={person} />}
-            </div>
+              <FriendsComponent person={person} key={person.first_name + person.last_name} />
           );
         })}
       </div>
